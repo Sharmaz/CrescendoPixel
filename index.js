79cf91920f8deddeaf9cbe9accce38c002478782
Sharmaz <irae45@gmail.com>
2016-01-01 17:03:01 -0600

    Servimos archivos estaticos

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict'
 const express = require('express')
 const web = express()
+const port = 8080
 var server
 
-server = web.listen(8080, function() {
-	console.log("Servidor arrancado")
+server = web.listen(port, function() {
+	console.log("Servidor arrancado en " + port)
 })
+
+web.use('/static', express.static('static'))
 
 web.get("/", function(req, res) {
 	res.sendfile("templates/home.html")
