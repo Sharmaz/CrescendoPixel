491d2a7fff2ad17d9e6cf9f2be172d26bc84630e
Sharmaz <irae45@gmail.com>
2016-01-01 18:28:29 -0600

    Modificamos el sistema de ficheros y ruteamos los archivos html, comenzamos a crear nuestro menu

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const port = 8080
 var server
 
 server = web.listen(port, function() {
-	console.log("Servidor arrancado en " + port)
+	console.log('Servidor arrancado en ' + port)
 })
 
 web.use('/static', express.static('static'))
 
-web.get("/", function(req, res) {
-	res.sendfile("templates/home.html")
+web.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html')
 })
+
+web.use('/apps', express.static('public/apps.html'))
+web.use('/web', express.static('public/web.html'))
+web.use('/diseno', express.static('public/disenio.html'))
+web.use('/marketing', express.static('public/marketing.html'))
+web.use('/portafolio', express.static('public/portafolio.html'))
+web.use('/contacto', express.static('public/contacto.html'))
