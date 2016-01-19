1307651bc5717b6e10fc4e64eabfe3faab943944
Sharmaz <irae45@gmail.com>
2016-01-19 16:45:09 -0600

    Modificado el contact form, agregamos los modulos de nodemailer y mailgun transport

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const web = express()
 const port = 8080
+const nodemailer = require('nodemailer')
+const mg = require('nodemailer-mailgun-transport')
+const parser = require('body-parser')
+
 var server
 
 server = web.listen(port, function() {
@@ -20,3 +24,35 @@ web.use('/diseno', express.static('public/disenio.html'))
 web.use('/marketing', express.static('public/marketing.html'))
 web.use('/portafolio', express.static('public/portafolio.html'))
 web.use('/contacto', express.static('public/contacto.html'))
+
+web.use( parser.urlencoded({ extended: true }))
+web.post('/enviar', function(req, res) {
+	var nombre = req.body.nombre
+	var mail = req.body.mail
+	var mensaje = req.body.mensaje
+	var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+	nodemailerMailgun.sendMail({
+	  from: mail,
+	  to: 'irae45@gmail.com', // An array if you have multiple recipients.
+	  subject: 'Crescendo Formulario',
+	  text: 'Hey, ' + nombre + ' Tiene un mensaje: ' + mensaje
+	}, function (err, info) {
+	  if (err) {
+	    console.log('Error: ' + err);
+	  }
+	  else {
+	    console.log('Response: ' + info);
+	  }
+	});
+
+	res.send("Nombre " + nombre + " " + mail + " " + mensaje)
+})
+
+var auth = {
+  auth: {
+    api_key: 'key-4cec4805d46b7c935457f2c405b43f5f',
+    domain: 'https://api.mailgun.net/v3/crescendopixel.com'
+  }
+}
+/*
+ */
